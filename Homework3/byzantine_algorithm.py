2f7171a29f4b6ef3d3921bfeceb0b951e4f1cc1b
Midiel Henriquez <71528908+Midielhg@users.noreply.github.com>
2023-10-28 00:51:36 +0000

    Completing homework3

--- Homework3/byzantine_algorithm.py
+++ Homework3/byzantine_algorithm.py
@@ -7,7 +7,7 @@ import rsa
 
 no_of_generals = 10
 rsa_size = 512
-byzantine_prob = .8
+byzantine_prob = .7
 delay = .0001
 i = 0
 lock = Lock() # lock for synchronizing the shared inbox
@@ -76,12 +76,13 @@ def thread_safe_print(string: str):
     plock.release()
 
 
-def send_message(message: Message
-                                 ):    # adds the message to a random index of the shared_inbox
+def send_message(message: Message):    # adds the message to a random index of the shared_inbox
     global lock
     lock.acquire()
     shared_inbox.insert(randrange(0, len(shared_inbox) + 1), message)
     lock.release()
+
+
 
 
     #shared_inbox.append(message)
@@ -140,39 +141,27 @@ def verify_signatures(recepient: int, message: Message) -> bool:
         return False    #current recepient has already signed this message, no need to sign again
     for (sig, public_key) in message.sig_set:
         if public_key not in generals_public_keys:
-            thread_safe_print("General #" + str(recepient) +
-                                                ": Verification failed: Reason: public_key " +
-                                                str(public_key)[:7] + "... is unknown!")
+            thread_safe_print("General #" + str(recepient) + ": Verification failed: Reason: public_key " + str(public_key)[:7] + "... is unknown!")
             return False
-        if not rsa_signature_verify(message.text, sig, public_key[1],
-                                                                public_key[0]):
-            thread_safe_print("General #" + str(recepient) +
-                                                ": Verification failed. Reason: signature " +
-                                                str(sig)[:7] + "... is not from " +
-                                                str(public_key)[:7] + "..." + str(public_key)[-7:] +
-                                                " on text " + message.text)
-            return False
+        if not rsa_signature_verify(message.text, sig, public_key[1],public_key[0]):thread_safe_print("General #" + str(recepient) +": Verification failed. Reason: signature " + str(sig)[:7] + "... is not from " +str(public_key)[:7] + "..." + str(public_key)[-7:] + " on text " + message.text)
+        return False
     return True
 
 
 def general_run(index: int):
     public_key = (generals[index].n, generals[index].e)
     if not index:    # the commander
-        sig_set = [(rsa_message_sign("attack", generals[0].d,
-                                                                 generals[0].n), public_key)]
+        sig_set = [(rsa_message_sign("attack", generals[0].d,generals[0].n), public_key)]
         content = ("attack", sig_set)
-        thread_safe_print("The commander " +
-                                            " is broadcastng the attack message...")
+        thread_safe_print("The commander " + " is broadcastng the attack message...")
         if not generals[0].byzantine:    #if commander is not byzantine
             send_to_all(False, 0, content)    # command everyone to attack
         else:    #if commander is byzantine
-            sig_set = [(rsa_message_sign("retreat", generals[0].d,
-                                                                     generals[0].n), public_key)]
+            sig_set = [(rsa_message_sign("retreat", generals[0].d,generals[0].n), public_key)]
             content2 = ("retreat", sig_set)
             send_to_group(False, 0, content, 1, no_of_generals // 2 + 1)
             # command half of the generals to attack
-            send_to_group(False, 0, content2, no_of_generals // 2 + 1,
-                                        no_of_generals)
+            send_to_group(False, 0, content2, no_of_generals // 2 + 1, no_of_generals)
             # command the other half to retreat
     else:
         output = None
@@ -189,8 +178,7 @@ def general_run(index: int):
                     text = "attack" if message.text == "retreat" else "retreat"
                 else:    # don't change the message
                     text = message.text
-                signature = rsa_message_sign(text, generals[index].d,
-                                                                         generals[index].n)
+                signature = rsa_message_sign(text, generals[index].d,generals[index].n)
                 sig_set.append((signature, public_key))
                 content = (text, sig_set)
                 if first:
@@ -198,27 +186,21 @@ def general_run(index: int):
                     first = False
                 elif output != message.text:
                     output = None
-                thread_safe_print("General #" + str(index) +
-                                                    " is relaying the message " + message.text +
-                                                    " by sending " + text)
+                thread_safe_print("General #" + str(index) + " is relaying the message " + message.text + " by sending " + text)
                 send_to_all(True, index, content)
         sleep(1)    # wait before consensus
         if not generals[index].byzantine:
-            thread_safe_print("Honest general " + str(index) + "'s output: " +
-                                                str(output))
+            thread_safe_print("Honest general " + str(index) + "'s output: " + str(output))
         else:
-            thread_safe_print("Byzantine general " + str(index) +
-                                                "'s output doesn't matter!")
+            thread_safe_print("Byzantine general " + str(index) + "'s output doesn't matter!")
 
 
 for k in range(no_of_generals):
     public_key, private_key = rsa.newkeys(rsa_size)
-    general = General(k, private_key.d, (public_key.n, public_key.e),
-                                        random() > (1 - byzantine_prob))
+    general = General(k, private_key.d, (public_key.n, public_key.e), random() > (1 - byzantine_prob))
     generals.append(general)
 
-Thread(target=general_run,
-             args=(0, )).start()    # commander's thread starts early
+Thread(target=general_run,args=(0, )).start()    # commander's thread starts early
 threads = []
 for k in range(1, no_of_generals):
     thread = Thread(target=general_run, args=(k, ))
@@ -231,3 +213,4 @@ if generals[0].byzantine:
 else:
     thread_safe_print("Commander is honest and wants to attack!")
 thread_safe_print("Are all honest generals in-consensus?")
+
